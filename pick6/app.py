bdc93a5f92ad16334efb482a6613a914f8e404ae
scottwilster <wilster.scott@gmail.com>
2020-09-24 14:57:01 -0400

    yay! a working table

--- pick6/app.py
+++ pick6/app.py
@@ -1,14 +1,19 @@
 from flask import Flask, jsonify, render_template
 
-from utils import sql_to_df
+from utils import sql_to_df, sql_to_list
 
 app = Flask(__name__)
+
+import collections
 
 
 @app.route("/")
 def hello():
     tbl_df = sql_to_df("picks_bet")
-    return render_template("index.html", latest_team_list=tbl_df.to_dict("index"))
+
+    return render_template(
+        "index.html", latest_team_list=[x for x in tbl_df.itertuples()]
+    )
 
 
 @app.route("/_pick_data")
